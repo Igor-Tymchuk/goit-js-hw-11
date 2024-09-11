71366d932a21345d1f9b72974b4271aaca66d13a
Igor Tymchuk <167434833+Igor-Tymchuk@users.noreply.github.com>
2024-09-11 20:39:59 +0300

    norm

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const key = "45921559-66c2b9b15ee0f1bc0f2b1e46c";
+const options = {
+    key: key,
+    q: null,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+}
+
 const form = document.querySelector('.searchImg');
 const handleForm = event => {
     event.preventDefault();
@@ -8,7 +17,10 @@ const handleForm = event => {
     if(!searchText) return iziToast.warning({
         message: "Input is empty!",
         position: "topRight",
-    })
+    });
+    options.q = searchText;
+    fetch('https://pixabay.com/api/', options);
+    
 }
 form.addEventListener('submit', handleForm)
 
